26e2529b9e3a41a5ac756fc13976dc4fa66d1ae4
AlvaroMarques <sc.poi458@gmail.com>
2019-07-07 19:50:30 -0300

    comments of code used as base deleted

--- lgtv.ino
+++ lgtv.ino
@@ -1,23 +1,4 @@
-
-/*
- * IRremote: IRsendRawDemo - demonstrates sending IR codes with sendRaw
- * An IR LED must be connected to Arduino PWM pin 3.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
- *
- * IRsendRawDemo - added by AnalysIR (via www.AnalysIR.com), 24 August 2015
- *
- * This example shows how to send a RAW signal using the IRremote library.
- * The example signal is actually a 32 bit NEC signal.
- * Remote Control button: LGTV Power On/Off. 
- * Hex Value: 0x20DF10EF, 32 bits
- * 
- * It is more efficient to use the sendNEC function to send NEC signals. 
- * Use of sendRaw here, serves only as an example of using the function.
- * 
- */
-
+//Pin 3 of Arduino PWM must be used as output
 
 #include <IRremote.h>
 
